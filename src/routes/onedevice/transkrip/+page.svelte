c520f53a961d5d165dcbb0150e8b1c8762fc8235
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-05-13 23:37:57 +0700

    search transkrip

--- src/routes/onedevice/transkrip/+page.svelte
+++ src/routes/onedevice/transkrip/+page.svelte
@@ -1,6 +1,26 @@
-<script>
+<script lang="ts">
 	import { transkripNilai } from '$lib/stores/akademik';
+	import type { ITranskripNilai } from '@binsarjr/apiamikomone/lib/typings/Response';
 	import { Block, BlockTitle, List, ListItem, Navbar, NavbarBackLink, Page } from 'konsta/svelte';
+
+	let transkrips: {
+		JmlSks: number;
+		Kode: string;
+		NamaMk: string;
+		NamaMkEn: string;
+		NamaSifatMk: string;
+		Nilai: string;
+		SifatMk: string;
+	}[] = [];
+	let cari = '';
+	$: transkrips =
+		$transkripNilai?.Transkrip.filter((transkrip) => {
+			if (!cari) return true;
+			return JSON.stringify(transkrip).toLowerCase().includes(cari.toLowerCase());
+		}) || [];
+	$: if ($transkripNilai) {
+		transkrips = $transkripNilai.Transkrip;
+	}
 </script>
 
 <Page>
@@ -17,8 +37,16 @@
 			<ListItem title="Jumlah SKS Wajib" after={$transkripNilai.SksWajib.toString()} />
 		</List>
 		<BlockTitle>Transkrip Nilai</BlockTitle>
+		<Block>
+			<input
+				bind:value={cari}
+				type="text"
+				placeholder="Cari Mata Kuliah"
+				class="p-4 rounded bg-white mx-auto w-full"
+			/>
+		</Block>
 		<List strongIos insetIos class="mb-20">
-			{#each $transkripNilai.Transkrip as transkrip}
+			{#each transkrips as transkrip}
 				<ListItem
 					title={transkrip.NamaMk}
 					header={transkrip.Kode}

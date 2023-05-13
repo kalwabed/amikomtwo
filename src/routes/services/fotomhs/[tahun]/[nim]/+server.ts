8a6ac61509437dea52a0c0d9cfd09300547b0b38
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-05-14 00:07:19 +0700

    cache control by last modified

---
+++ src/routes/services/fotomhs/[tahun]/[nim]/+server.ts
@@ -0,0 +1,22 @@
+import type { RequestHandler } from "./$types"
+
+export const GET: RequestHandler = async ({ params, setHeaders }) => {
+    const tahun = params.tahun
+    const nim = params.nim.replace(/\./g, '_')
+    const link = `https://fotomhs.amikom.ac.id/${tahun}/${nim}.jpg`
+
+    const response = await fetch(link)
+    const headers = Object.fromEntries(response.headers)
+    console.log(headers)
+
+    const lastModified: any = new Date(headers['last-modified'])
+    const cacheControl = `max-age=${Math.floor((Date.now() - lastModified) / 1000)}, must-revalidate`;
+
+    headers['cache-control'] = cacheControl
+    headers['Content-Disposition'] = `filename="${nim}.jpg"`
+    
+    setHeaders(headers)
+    const imageBlob = Buffer.from(await response.arrayBuffer())
+
+    return new Response(imageBlob)
+}
